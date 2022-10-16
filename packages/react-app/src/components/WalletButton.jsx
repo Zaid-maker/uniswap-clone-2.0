0eae892a3596f512c641fcbc944cae8ccd237dd1
Zaid-maker <pzhafeez@gmail.com>
2022-10-16 19:44:04 +0500

    update wallet button

--- packages/react-app/src/components/WalletButton.jsx
+++ packages/react-app/src/components/WalletButton.jsx
@@ -1,30 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { shortenAddress, useEthers, useLookupAddress } from '@usedapp/core'
-import styles from '../styles'
+import React, { useState, useEffect } from "react";
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+import styles from "../styles";
 
 const WalletButton = () => {
-  const [accountAddress, setaccountAddress] = useState('')
-  const { ens } = useLookupAddress()
-  const { account, activateBrowserWallet, deactivate } = useEthers()
+  const [accountAddress, setAccountAddress] = useState("");
+  const { ens } = useLookupAddress();
+  const { account, activateBrowserWallet, deactivate } = useEthers();
 
+  /* A hook that is called when the component is mounted and when the component is updated. */
   useEffect(() => {
-
-  }, [account, ens, setaccountAddress])
+    if (ens) {
+      setAccountAddress(ens);
+    } else if (account) {
+      setAccountAddress(shortenAddress(account));
+    } else {
+      setAccountAddress('');
+    }
+  }, [account, ens, setAccountAddress]);
 
   return (
     <button
+      /* Checking if the account is active or not. If it is not active, it will activate the browser
+      wallet. If it is active, it will deactivate the browser wallet. */
       onClick={() => {
         if (!account) {
-          activateBrowserWallet()
+          activateBrowserWallet();
         } else {
-          deactivate()
+          deactivate();
         }
       }}
       className={styles.walletButton}
     >
       {!accountAddress ? "Connect Wallet" : accountAddress}
     </button>
-  )
-}
+  );
+};
 
-export default WalletButton
+export default WalletButton;
