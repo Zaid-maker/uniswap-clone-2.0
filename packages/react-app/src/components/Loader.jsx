eddbd1248d73301588dfb9a1a4c256477897b7ce
Zaid-maker <pzhafeez@gmail.com>
2022-10-16 17:37:57 +0500

    Loader component

--- packages/react-app/src/components/Loader.jsx
+++ packages/react-app/src/components/Loader.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import styles from '../styles'
+import { ethereumLogo } from '../assets'
+
 const Loader = ({ title }) => {
   return (
-    <div>Loader</div>
+    <div className={styles.loader}>
+      <img
+        src={ethereumLogo}
+        alt="ethereumLogo"
+        className={styles.loaderImg}
+      />
+      <p className={styles.loaderText}>{title}</p>
+    </div>
   )
 }
 
