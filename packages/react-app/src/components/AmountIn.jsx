49f135219062cb0df4dd374a48ee646f1c23e999
Zaid-maker <pzhafeez@gmail.com>
2022-10-17 16:52:03 +0500

    Exchange component got new things

---
+++ packages/react-app/src/components/AmountIn.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import { chevronDown } from "../assets";
+import styles from "../styles";
+
+const AmountIn = () => {
+  const [showList, setShowList] = useState(false);
+
+  return (
+    <div className={styles.amountContainer}>
+      <input
+        placeholder="0.0"
+        type="number"
+        value=""
+        disabled={false}
+        onClick={() => {}}
+        className={styles.amountInput}
+      />
+
+      <div
+        className="relative"
+        onClick={() => setShowList((prevState) => !prevState)}
+      >
+        <button className={styles.currencyButton}>
+          {"ETH"}
+          <img
+            src={chevronDown}
+            alt="chevronDown"
+            className={`w-4 h-4 object-contain ml-2 ${
+              showList ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+
+        {showList && (
+          <ul className={styles.currencyList}>
+            {[
+              {
+                token: "ETH",
+                tokenName: "ETH",
+              },
+              {
+                token: "USD",
+                tokenName: "USD",
+              },
+            ].map(({ token, tokenName }, index) => (
+              <li
+                key={index}
+                className={`${styles.currencyListItem} ${
+                  true ? "bg-site-dim-2" : ""
+                } cursor-pointer`}
+              >
+                {tokenName}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AmountIn;
--- packages/react-app/src/components/Exchange.jsx
+++ packages/react-app/src/components/Exchange.jsx
@@ -11,11 +11,23 @@ import {
 } from "@usedapp/core";
 import { ethers } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
+import { AmountIn, AmountOut, Balance } from '../components'
 
 import { ROUTER_ADDRESS } from '../config'
 
 const Exchange = ({ pools }) => {
-  return <div>Exchange</div>;
+  return (
+    <div className="flex flex-col w-full items-center">
+      <div className="mb-8">
+        <AmountIn />
+        <Balance />
+      </div>
+      <div className="mb-8 w-[100%]">
+        <AmountOut />
+        <Balance />
+      </div>
+    </div>
+  );
 };
 
 export default Exchange;
