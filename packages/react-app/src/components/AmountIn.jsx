7636ccb45a62f369c0cc710fc092aeeab8ef9818
Zaid-maker <pzhafeez@gmail.com>
2022-10-19 22:01:49 +0500

    make amount in dynamic

--- packages/react-app/src/components/AmountIn.jsx
+++ packages/react-app/src/components/AmountIn.jsx
@@ -1,28 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import { chevronDown } from "../assets";
+import { useOnClickOutside } from '../utils'
 import styles from "../styles";
 
-const AmountIn = () => {
+const AmountIn = ({ value, onChange, currencyValue, onSelect, currencies, isSwapping }) => {
   const [showList, setShowList] = useState(false);
+  const [activeCurrency, setActiveCurrency] = useState("Select")
+  const ref = useRef()
+
+  useOnClickOutside(ref, () => setShowList(false))
+
+  useEffect(() => {
+    if (Object.keys(currencies).includes(currencyValue))setActiveCurrency(currencies[currencyValue]);
+    else setActiveCurrency("Select")
+  }, [currencies, currencyValue])
 
   return (
     <div className={styles.amountContainer}>
       <input
         placeholder="0.0"
         type="number"
-        value=""
-        disabled={false}
-        onClick={() => {}}
+        value={value}
+        disabled={isSwapping}
+        onChange={(e) => typeof onChange === "function" && onChange(e.target.value)}
         className={styles.amountInput}
       />
 
       <div
         className="relative"
-        onClick={() => setShowList((prevState) => !prevState)}
+        onClick={() => setShowList(!showList)}
       >
         <button className={styles.currencyButton}>
-          {"ETH"}
+          {activeCurrency}
           <img
             src={chevronDown}
             alt="chevronDown"
@@ -33,22 +43,18 @@ const AmountIn = () => {
         </button>
 
         {showList && (
-          <ul className={styles.currencyList}>
-            {[
-              {
-                token: "ETH",
-                tokenName: "ETH",
-              },
-              {
-                token: "USD",
-                tokenName: "USD",
-              },
-            ].map(({ token, tokenName }, index) => (
+          <ul ref={ref} className={styles.currencyList}>
+            {Object.entries(currencies).map(([token, tokenName], index) => (
               <li
                 key={index}
                 className={`${styles.currencyListItem} ${
-                  true ? "bg-site-dim-2" : ""
+                  activeCurrency === tokenName ? "bg-site-dim2" : ""
                 } cursor-pointer`}
+                onClick={() => {
+                  if (typeof onSelect === "function") onSelect(token);
+                  setActiveCurrency(tokenName);
+                  setShowList(false);
+                }}
               >
                 {tokenName}
               </li>
