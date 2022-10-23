2870fabbced7bad45371d7fbe2a3d69a93c156fc
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-23 14:48:58 +0000

    update amount out component

--- packages/react-app/src/components/AmountOut.jsx
+++ packages/react-app/src/components/AmountOut.jsx
@@ -1,30 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { formatUnits } from "ethers/lib/utils";
 
 import { chevronDown } from "../assets";
+import { useAmountsOut, useOnClickOutside } from "../utils";
 import styles from "../styles";
 
-const AmountOut = () => {
+const AmountOut = ({
+  fromToken,
+  toToken,
+  amountIn,
+  pairContract,
+  currencyValue,
+  onSelect,
+  currencies,
+}) => {
   const [showList, setShowList] = useState(false);
+  const [activeCurrency, setActiveCurrency] = useState("Select");
+  const ref = useRef();
+
+  const amountOut =
+    useAmountsOut(pairContract, amountIn, fromToken, toToken) ?? 0;
+
+  useOnClickOutside(ref, () => setShowList(false));
+
+  useEffect(() => {
+    if (Object.keys(currencies).includes(currencyValue)) {
+      setActiveCurrency(currencies[currencyValue]);
+    } else {
+      setActiveCurrency("Select");
+    }
+  }, [currencyValue, currencies]);
 
   return (
     <div className={styles.amountContainer}>
       <input
         placeholder="0.0"
         type="number"
-        value=""
-        disabled
+        value={formatUnits(amountOut)}
         className={styles.amountInput}
+        disabled
       />
 
-      <div
-        className="relative"
-        onClick={() => setShowList((prevState) => !prevState)}
-      >
+      <div className="relative" onClick={() => setShowList(!showList)}>
         <button className={styles.currencyButton}>
-          {"ETH"}
+          {activeCurrency}
           <img
             src={chevronDown}
-            alt="chevronDown"
+            alt="cheveron-down"
             className={`w-4 h-4 object-contain ml-2 ${
               showList ? "rotate-180" : "rotate-0"
             }`}
@@ -32,22 +54,16 @@ const AmountOut = () => {
         </button>
 
         {showList && (
-          <ul className={styles.currencyList}>
-            {[
-              {
-                token: "ETH",
-                tokenName: "ETH",
-              },
-              {
-                token: "USD",
-                tokenName: "USD",
-              },
-            ].map(({ token, tokenName }, index) => (
+          <ul ref={ref} className={styles.currencyList}>
+            {Object.entries(currencies).map(([token, tokenName], index) => (
               <li
                 key={index}
-                className={`${styles.currencyListItem} ${
-                  true ? "bg-site-dim-2" : ""
-                } cursor-pointer`}
+                className={styles.currencyListItem}
+                onClick={() => {
+                  if (typeof onSelect === "function") onSelect(token);
+                  setActiveCurrency(tokenName);
+                  setShowList(false);
+                }}
               >
                 {tokenName}
               </li>
